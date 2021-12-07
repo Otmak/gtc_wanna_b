93663d1caf57dc0dce5b34b7a61324153f62a87d
OT <jakelovz@gmail.com>
2021-12-07 13:36:04 -0800

    an attempted ...

---
+++ api/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/main', methods=['POST', 'GET'])
+def main():
+
+    try:
+        if request.method == 'GET':
+            print('Method is get')
+            return 'Method is get'
+        if request.method == 'POST':
+            print('Method is post')
+            return 'Method is post'
+
+    except :
+        return { 'error': 'Error' }
+
+
+@app.route('/path', methods=['POST'])
+def path():
+
+    pass
