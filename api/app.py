3c1d11a725ce7f4bfee0dea6523e3986b57ab114
OT <jakelovz@gmail.com>
2021-12-23 01:15:22 -0800

    1.01 clean up in app.py

--- api/app.py
+++ api/app.py
@@ -9,20 +9,18 @@ app = Flask(__name__)
 asset_url = 'https://omi.zonarsystems.net/interface.php?customer=&username=&password=&action=showopen&operation=showassets&format=xml'
 path_url = 'https://omi.zonarsystems.net/interface.php?customer=&username=&password=&action=showposition&operation=path&reqtype=dbid&target=191&version=2&starttime=1189321200&endtime=1189493940&logvers=3&format=xml'
 gps_url = 'https://omi.zonarsystems.net/interface.php?customer=&username=&password=&action=showopen&operation=showgps&format=xml'
-# test_asset_api = f'https://omi.zonarsystems.net/interface.php?customer={customer}&username={z}&password={px}&action=showopen&operation=showassets&format=xml'
 
 
-# Take user login input and api string as argurment and and insert the login credential then return complete URL.  (-_-) too long***
+# Generate API
 def generate_api(user, api):
-    print('Generating API........')
+
     split_api = api.split('=')  # split the Link or string, separating it by '='
     fully_built_api = ''
 
     for i in split_api:
-
         try:
             if len(i) == 51:
-                fully_built_api = i + '=' + user['account']
+                fully_built_api = i + '=' + user['companyid']
             elif i == '&password':
                 fully_built_api += i + '=' + user['password']
             elif i == '&username':
@@ -32,60 +30,67 @@ def generate_api(user, api):
         except:
             return 'Encoutered some Error'
 
-    print(fully_built_api[:-1])
-
     return fully_built_api[:-1]
 
 
-# Take a api url as argument and return byte data if call is successful
+# Takes a api url as argument and returns byte data if call is successful 
 def make_call(url):
+
     try:
         r = requests.get(url)
         if r.ok:
-            print('Request success!.......', r.status_code)
             return r.content
         else:
-            return unpack_and_interpret(r.content)
+            return r.content
     except TypeError:
-        print('Some Error occured')
-        return {'error': 'got a type error'}
+        return {'error': 'something went wrong with the type of call'}
 
 
-# Deside
 def atrrib_or_text(elem):
-    if elem.text and elem.attrib:
-        data = elem.attrib
-        data[elem.tag] = elem.text
-        return data
-    elif elem.text is not None and len(elem.findall('*')) <= 0:
-        return str(elem.text)
-    elif elem.attrib != None and len(elem.attrib) != 0:
-        return elem.attrib
-    elif len(elem.findall('*')) > 1:
-        return get_elems(elem)
-    else:
-        if elem.text is not None and len(elem.text) > 0:
-            return elem.text
-        else:
-            return 'TBD'
+    '''
+    filter: nothing,
+            text,
+            attrib,
+            text & Attrib,
+            more childs
+    '''
+
+    if elem.text == None and len(elem.attrib) < 1: # checking for tag with no data
+      return None
+
+    elif elem.text is not None and len(elem.attrib) < 1 and len(elem.findall('*')) < 1: # checking for text only
+      return elem.text
+
+    elif len(elem.attrib) > 0 and elem.text is None: # checking for attribs only (may not exist)
+      return elem.attrib
+
+    elif len(elem.attrib) > 0 and elem.text is not None: # checking for Text & Attrib
+      return {'attrib': elem.attrib, elem.tag : elem.text}
+
+    elif len(elem.findall('*')) > 1 : # checking for children
+      return get_elems(elem)
 
 
-
-# Take XML data and return a python dictiony
+# Takes XML data and returns a python dictiony/ for elems without sub elems
 def get_elems(root):
+
     main_data = {}
     for child in root.findall('*'):
         main_data[child.tag] = atrrib_or_text(child)
+
     return main_data
 
 
-# Take XML tree and initiate the creation of our main python dictionary
+# takes XML or dict data and if type is XML convert to dict only needed if elem has sub elems
 def init_dictionary(data):
-    main_data = {}
-    print('The type of data is ....', type(data), data.tag)
-    if data.tag == 'error':
-        return get_elems(data)
 
+    if isinstance(data, dict):
+      return data
+
+    if data.tag == 'error':
+      return get_elems(data)
+
+    main_data = {}
     for i in data:
         main_data[i.get('id')] = {
             i.tag: i.attrib,
@@ -94,39 +99,27 @@ def init_dictionary(data):
     return main_data
 
 
-# Take byte data from api call and convert it then return a XML tree
-def unpack_and_interpret(payload):
+# takes byte data from api call and converts it and returns a XML tree 
+def unpack_bytes(payload):
+
     if isinstance(payload, bytes):
-        data = ET.fromstring(payload)
-        print('returning data.........')
-        return data
+      data = ET.fromstring(payload)  # Convert data to from XML tree
+      return data
     else:
-        print('type of not byte', payload)
-        return {'error': 'input not bytes'}
+      return {'massage' : 'input not bytes', 'data': payload}
 
 
-# @app.route('/main', methods=['POST', 'GET'])
-# def main():
+# print(  init_dictionary( unpack_bytes(make_call(generate_api(u_data, asset_url))) ) ) 
 
-#     try:
-#         if request.method == 'GET':
-#             print('Method is get')
-#             return 'Method is get'
-#         if request.method == 'POST':
-#             print('Method is post')
-#             return 'Method is post'
 
-#     except :
-#         return { 'error': 'Error' }
 @app.route('/')
 def index():
-    return 'home'
+    return 'Success!'
+
 
 @app.route('/dash')
 def dash():
     return {'online':200}
-
-
 
 
 @app.route('/test', methods=['POST'])
